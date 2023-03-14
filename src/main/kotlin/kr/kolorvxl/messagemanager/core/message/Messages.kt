9e24a23fc7c2a2eb86548ddc4617f5b6c68f2544
kolorVXL <kolorvxl@gmail.com>
2023-03-15 01:19:25 +0900

    edit: linebreak

--- src/main/kotlin/kr/kolorvxl/messagemanager/core/message/Messages.kt
+++ src/main/kotlin/kr/kolorvxl/messagemanager/core/message/Messages.kt
@@ -2,5 +2,7 @@ package kr.kolorvxl.messagemanager.core.message
 
 sealed class Messages(open val name: String)
 
+
 data class MessageGroup(override val name: String, val subMessages: List<Messages>) : Messages(name)
+
 data class Message(override val name: String) : Messages(name)
