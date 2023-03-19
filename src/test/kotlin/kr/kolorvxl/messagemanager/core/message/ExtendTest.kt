3243014ebddc3acf93418e929aeb5d8207811876
kolorVXL <kolorvxl@gmail.com>
2023-03-19 21:45:54 +0900

    add: ExtendTest

---
+++ src/test/kotlin/kr/kolorvxl/messagemanager/core/message/ExtendTest.kt
@@ -0,0 +1,21 @@
+package kr.kolorvxl.messagemanager.core.message
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertTrue
+
+class ExtendTest {
+
+    @Test
+    fun `extend test`() {
+        val superSuper: SuperSuper = DuperDuper()
+        assertTrue(superSuper.thisObject() is DuperDuper)
+    }
+
+}
+
+
+open class SuperSuper {
+    fun thisObject() = this
+}
+
+class DuperDuper : SuperSuper()
