1cc2e93d74dba077faddf5ff31f1bbd861778516
kolorVXL <kolorvxl@gmail.com>
2023-03-19 14:14:05 +0900

    add: MessageFormatter

--- src/main/kotlin/kr/kolorvxl/messagemanager/core/message/MessageFormatter.kt
+++ src/main/kotlin/kr/kolorvxl/messagemanager/core/message/MessageFormatter.kt
@@ -1,7 +1,19 @@
 package kr.kolorvxl.messagemanager.core.message
 
-abstract class MessageFormatter<E : Enum<E>, M, R>(
-    messageStorage: MessageStorage<E, M>
-) {
+data class FormatInformation<E : Enum<E>, M>(
+    val storage: MessageStorage<E, M>,
+    val language: Enum<E>
+)
+
+abstract class MessageFormatter<E : Enum<E>, M, I, O, R> {
+
+    abstract fun format(
+        messageStorage: MessageStorage<E, M>,
+        language: Enum<E>,
+        messageType: MessageType,
+        function: MessageFormatter<E, M, I, O, R>.() -> Unit
+    ): R
+
+    abstract fun replace(from: I, to: O)
 
 }
