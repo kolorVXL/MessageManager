e92df114f9a6106500233704f754f5efbfc90dc2
kolorVXL <kolorvxl@gmail.com>
2023-03-20 22:50:02 +0900

    change: Any.staticMessages to property

--- src/main/kotlin/kr/kolorvxl/messagemanager/api/message/StaticMessages.kt
+++ src/main/kotlin/kr/kolorvxl/messagemanager/api/message/StaticMessages.kt
@@ -6,33 +6,33 @@ import kr.kolorvxl.messagemanager.core.message.MessageTypesWrapper
 /**
  * The message set interface. You can include [StaticMessage] in this.
  */
-class StaticMessageSet : MessageTypesWrapper {
+open class StaticMessageSet : MessageTypesWrapper {
 
     override fun toMessageTypes(): List<MessageType> =
         this::class
             .nestedClasses
-            .getStaticMessages()
+            .staticMessages
             .map {
                 val messageType = MessageType(it.first)
                 it.second.messageType = messageType
                 messageType
             }
 
-    private fun Any.getStaticMessages(): List<Pair<List<String>, StaticMessage>> =
-        this::class
-            .nestedClasses
-            .filter { it.objectInstance != null }
-            .flatMap { klass ->
-                val instance = klass.objectInstance!!
-                val name = listOf(klass.simpleName ?: "")
-                if (instance is StaticMessage) {
-                    listOf(Pair(name, instance))
-                } else {
-                    klass.getStaticMessages().map { Pair(name + it.first, it.second) }
-                }
-            }
-
 }
+
+val Any.staticMessages: List<Pair<List<String>, StaticMessage>>
+    get() = this::class
+        .nestedClasses
+        .filter { it.objectInstance != null }
+        .flatMap { klass ->
+            val instance = klass.objectInstance!!
+            val name = listOf(klass.simpleName ?: "")
+            if (instance is StaticMessage) {
+                listOf(Pair(name, instance))
+            } else {
+                instance.staticMessages.map { Pair(name + it.first, it.second) }
+            }
+        }
 
 /**
  * The single message class.
---
+++ src/test/kotlin/kr/kolorvxl/messagemanager/api/message/StaticMessageTest.kt
@@ -0,0 +1,43 @@
+package kr.kolorvxl.messagemanager.api.message
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+
+object TestingSet : StaticMessageSet() {
+
+    object Test1 {
+
+        object Test3 : End()
+
+        object SubTest1 {
+
+            object SubTest2 : End()
+
+        }
+
+    }
+
+    object Test2 {
+
+        object SubTest3 : End()
+
+    }
+
+    object Test3 : End()
+
+}
+
+
+class StaticMessageTest {
+
+    @Test
+    fun `static message test`() {
+            TestingSet
+                .staticMessages
+                .map {
+                    "${it.first.reduce { a, b -> "$a.$b" }}: ${it.second}"
+                }.forEach(::println)
+    }
+
+}
