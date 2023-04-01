8d7a9b389af363d854d227a4f330cf9b7f77a01a
kolorVXL <kolorvxl@gmail.com>
2023-04-01 16:38:51 +0900

    impl: part of BukkitMessageFormatter

--- src/main/kotlin/kr/kolorvxl/messagemanager/bukkit/message/BukkitMessageFormatter.kt
+++ src/main/kotlin/kr/kolorvxl/messagemanager/bukkit/message/BukkitMessageFormatter.kt
@@ -1,20 +1,64 @@
 package kr.kolorvxl.messagemanager.bukkit.message
 
 import kr.kolorvxl.messagemanager.core.message.MessageFormatter
+import net.md_5.bungee.api.ChatColor
 import net.md_5.bungee.api.chat.BaseComponent
+import net.md_5.bungee.api.chat.ComponentBuilder
 
-class BukkitMessageFormatter : MessageFormatter<List<BaseComponent>, BukkitMessageFormatter>() {
+class BukkitMessageFormatter : MessageFormatter<Array<BaseComponent>, BukkitMessageFormatter>() {
 
-    override fun format(string: String, function: BukkitMessageFormatter.() -> Unit): List<BaseComponent> {
+    var color: ChatColor? = null
+    var bold: Boolean? = null
+    var italic: Boolean? = null
+    var underline: Boolean? = null
+    var strikethrough: Boolean? = null
+    var obfuscate: Boolean? = null
+    var font: String? = null
+
+    private var innerValue: List<InnerMessage> = emptyList()
+
+    override fun format(string: String, function: BukkitMessageFormatter.() -> Unit): Array<BaseComponent> {
+        innerValue = listOf(InnerPlainMessage(string))
+        function()
+
+        val builder = ComponentBuilder()
+        innerValue
+            .map {
+                when (it) {
+                    is InnerPlainMessage -> it.value.toComponents()
+                    is InnerComponentsMessage -> it.components
+                }
+            }
+            .forEach(builder::append)
+        return builder.create()
+    }
+
+    override fun String.to(r: Array<BaseComponent>) {
         TODO("Not yet implemented")
     }
 
-    override fun String.to(r: List<BaseComponent>) {
-        TODO("Not yet implemented")
+    override fun List<Array<BaseComponent>>.concat(): Array<BaseComponent> {
+        val builder = ComponentBuilder()
+        this.forEach(builder::append)
+        return builder.create()
     }
 
-    override fun List<List<BaseComponent>>.concat(): List<BaseComponent> {
-        TODO("Not yet implemented")
+    private fun String.toComponents(): Array<BaseComponent> {
+        val builder = ComponentBuilder(this)
+
+        color?.let(builder::color)
+        bold?.let(builder::bold)
+        italic?.let(builder::italic)
+        underline?.let(builder::underlined)
+        strikethrough?.let(builder::strikethrough)
+        obfuscate?.let(builder::obfuscated)
+        font?.let(builder::font)
+
+        return builder.create()
     }
+
+    sealed interface InnerMessage
+    class InnerPlainMessage(val value: String) : InnerMessage
+    class InnerComponentsMessage(val components: Array<BaseComponent>) : InnerMessage
 
 }
---
+++ src/main/kotlin/kr/kolorvxl/messagemanager/bukkit/message/BukkitMessageStorages.kt
@@ -0,0 +1,2 @@
+package kr.kolorvxl.messagemanager.bukkit.message
+
