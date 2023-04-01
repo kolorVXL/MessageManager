ff8dfb8302da86ea497c875631a6437922afe148
kolorVXL <kolorvxl@gmail.com>
2023-04-01 21:20:12 +0900

    add: FormalMessageStorage

--- src/main/kotlin/kr/kolorvxl/messagemanager/core/MessageStorages.kt
+++ src/main/kotlin/kr/kolorvxl/messagemanager/core/MessageStorages.kt
@@ -1,11 +1,30 @@
 package kr.kolorvxl.messagemanager.core
 
+
 abstract class MessageStorage<E : Enum<E>>(
     languages: Class<Enum<E>>, messageTypesWrapper: MessageTypesWrapper
 ) {
     abstract fun get(languageType: Enum<E>): SingleMessageStorage
 }
 
-data class SingleMessageStorage(val values: List<Message>) {
+
+abstract class FormalMessageStorage<E : Enum<E>>(
+    languages: Class<Enum<E>>, messageTypesWrapper: MessageTypesWrapper
+) : MessageStorage<E>(languages, messageTypesWrapper) {
+
+    abstract val values: List<SingleMessageStorage>
+
+    override fun get(languageType: Enum<E>): SingleMessageStorage = values[languageType.ordinal]
+
+}
+
+
+data class SingleMessageStorage(val values: Array<Message>) {
+
     operator fun get(messageType: MessageType) = messageType.identifier?.let { values[it] }
+
+    override fun equals(other: Any?) = values.contentEquals((other as? SingleMessageStorage)?.values)
+
+    override fun hashCode() = values.contentHashCode()
+
 }
