1ea4d533f77bb09cd91da1760f98d5e866eb556e
kolorVXL <kolorvxl@gmail.com>
2023-04-16 04:31:35 +0900

    fix: import error

--- src/test/kotlin/kr/kolorvxl/messagemanager/util/KillAnnoyingWarning.kt
+++ src/test/kotlin/kr/kolorvxl/messagemanager/util/KillAnnoyingWarning.kt
@@ -1,13 +1,11 @@
 package kr.kolorvxl.messagemanager.util
 
-import kr.kolorvxl.messagemanager.core.typeset.DynamicMessageTypeSet
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class KillAnnoyingWarning {
     @Test
     fun hi() {
-        DynamicMessageTypeSet{}.plainMessageTypes
         assertEquals(1, 1)
     }
 }
