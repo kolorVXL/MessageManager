c2378821314138f76ae0d68d7365aa723dd7436c
kolorVXL <kolorvxl@gmail.com>
2023-04-25 23:39:12 +0900

    delete: broker

--- src/main/kotlin/kr/kolorvxl/messagemanager/core/broker/ExtensionSender.kt
+++
@@ -1,8 +0,0 @@
-package kr.kolorvxl.messagemanager.core.broker
-
-interface ExtensionSender<R, I> : ResultSender<R> {
-    val inner: I
-}
-
-interface NationalExtensionSender<R, L : Enum<L>, I>
-    : NationalResultSender<R, L>, ExtensionSender<R, I>
