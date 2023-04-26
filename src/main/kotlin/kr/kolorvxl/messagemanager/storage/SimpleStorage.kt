0d992d5855feed995b64b0031ad871f7421d7ece
kolorVXL <kolorvxl@gmail.com>
2023-04-27 01:45:21 +0900

    add: SimpleStorage

---
+++ src/main/kotlin/kr/kolorvxl/messagemanager/storage/SimpleStorage.kt
@@ -0,0 +1,20 @@
+package kr.kolorvxl.messagemanager.storage
+
+import kr.kolorvxl.messagemanager.formatter.Formatter
+import kr.kolorvxl.messagemanager.language.Language
+import kr.kolorvxl.messagemanager.message.Message
+
+abstract class SimpleStorage<R, F : Formatter<R, F>> {
+
+    abstract val storage: List<List<String>>
+
+    abstract val language: Language
+
+    abstract val formatter: Formatter<R, F>
+
+
+    fun format(plain: String, commands: F.() -> Unit) = formatter.copy().format(plain, commands)
+
+    fun get(message: Message): String = storage[language.id][message.id]
+
+}
