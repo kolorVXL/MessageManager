9cf584179b205d66f2985e1112cf545383212f7e
kolorVXL <kolorvxl@gmail.com>
2023-05-16 01:19:28 +0900

    add: setClickEvent, setHoverEvent

--- src/main/kotlin/kr/kolorvxl/messagemanager/bukkit/formatter/BukkitFormatter.kt
+++ src/main/kotlin/kr/kolorvxl/messagemanager/bukkit/formatter/BukkitFormatter.kt
@@ -3,7 +3,11 @@ package kr.kolorvxl.messagemanager.bukkit.formatter
 import kr.kolorvxl.messagemanager.formatter.FormatterImpl
 import net.md_5.bungee.api.ChatColor
 import net.md_5.bungee.api.chat.BaseComponent
+import net.md_5.bungee.api.chat.ClickEvent
 import net.md_5.bungee.api.chat.ComponentBuilder
+import net.md_5.bungee.api.chat.HoverEvent
+import net.md_5.bungee.api.chat.hover.content.Content
+import net.md_5.bungee.api.chat.hover.content.Text
 import java.awt.Color
 
 class BukkitFormatter : FormatterImpl<Array<BaseComponent>, BukkitFormatter>() {
@@ -16,6 +20,9 @@ class BukkitFormatter : FormatterImpl<Array<BaseComponent>, BukkitFormatter>() {
     var obfuscate: Boolean? = null
     var font: String? = null
 
+    var clickEvent: ClickEvent? = null
+    var hoverEvent: HoverEvent? = null
+
     override fun result(plain: String): Array<BaseComponent> {
         val builder = ComponentBuilder(plain)
 
@@ -26,6 +33,9 @@ class BukkitFormatter : FormatterImpl<Array<BaseComponent>, BukkitFormatter>() {
         strikethrough?.let(builder::strikethrough)
         obfuscate?.let(builder::obfuscated)
         font?.let(builder::font)
+
+        clickEvent?.let(builder::event)
+        hoverEvent?.let(builder::event)
 
         return builder.create()
     }
@@ -40,5 +50,16 @@ class BukkitFormatter : FormatterImpl<Array<BaseComponent>, BukkitFormatter>() {
 
     fun color(str: String): ChatColor = ChatColor.of(str)
     fun color(color: Color): ChatColor = ChatColor.of(color)
+
+    fun openUrl(url: String) = ClickEvent(ClickEvent.Action.OPEN_URL, url)
+    fun openFile(file: String) = ClickEvent(ClickEvent.Action.OPEN_FILE, file)
+    fun runCommand(command: String) = ClickEvent(ClickEvent.Action.RUN_COMMAND, command)
+    fun suggestCommand(command: String) = ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, command)
+    fun changePage(command: String) = ClickEvent(ClickEvent.Action.CHANGE_PAGE, command)
+
+    fun showText(vararg content: Content) = HoverEvent(HoverEvent.Action.SHOW_TEXT, *content)
+    fun showText(component: Array<BaseComponent>) = showText(Text(component))
+    fun showItem(vararg content: Content) = HoverEvent(HoverEvent.Action.SHOW_ITEM, *content)
+    fun showEntity(vararg content: Content) = HoverEvent(HoverEvent.Action.SHOW_ENTITY, *content)
 
 }
